97a83ec52e53a572554758850080f3bb2be62b72
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2022-09-01 08:34:36 -0500

    Week2

---
+++ Week2/build/CMakeFiles/week_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week_2.dir/src/main.c.o"
+  "CMakeFiles/week_2.dir/src/main.c.o.d"
+  "week_2"
+  "week_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/week_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

c6d83d4c8e56070308e3c075f2a04eb08c5c295a
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2022-09-06 08:01:47 -0500

    added cnl bin

--- Week2/src/main.c
+++ Week2/src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char ** argv){
+    int i;
+    char c;
+    double ft;
 
+    scanf("%d %lf", &i, &ft);
+
+    printf("%d %12.9lf\n", i, ft);
 
     return 0;
 }

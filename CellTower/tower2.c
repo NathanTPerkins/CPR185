ccb9cb31b252bfc5c67592c49c5c00c81c80d479
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2022-09-13 08:44:08 -0500

    Added cell tower function

--- CellTower/tower2.c
+++ CellTower/tower2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+double heightClearance(double startHt, double endHt, double distEnd, double d){
+    double x = (d/distEnd)*(endHt-startHt);
+    return x + startHt;
+}
+
 
 int main(int argc, char ** argv){
     double hAnt, dAnt, dTree, hTower;
@@ -8,7 +13,7 @@ int main(int argc, char ** argv){
     printf(" Enter antenna ht, distance to antenna, distance to tree, and Tower ht:\n");
     scanf(" %lf %lf %lf %lf", &hAnt, &dAnt, &dTree, &hTower);
 
-    hTree = (dTree/dAnt) * (hAnt - hTower);
-    hTree += hTower;
+    hTree = heightClearance(hTower, hAnt, dAnt, dTree);
+
     printf("you can clear a %.3lf meter tree at %.3lf meters.\n", hTree, dTree);
 }

25c638aa90620746b8c660cbe7d75d5ccd568816
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2022-09-15 08:51:24 -0500

    Added printTime Function

--- TimeBetweenTimes/timediff.c
+++ TimeBetweenTimes/timediff.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void printTime(int, int, double, char);
+
 int main(){
     int h1, m1;
     double s1;
@@ -7,5 +9,11 @@ int main(){
 
     scanf("%d:%d:%lf %cM", &h1, &m1, &s1, &ampm);
 
+    printTime(h1, m1, s1, ampm);
+
     return 0;
+}
+
+void printTime(int h, int m, double s, char ampm){
+    printf("%2d:%2d:%6.3lf %cM\n", h, m, s, ampm);
 }

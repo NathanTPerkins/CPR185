72118e36ac2a16828a9485ea3de26b94d8c6aaa3
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2022-10-18 08:38:31 -0500

    Array Practice

--- MakingImages/main.c
+++ MakingImages/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    puts("Cool thing that is cool!");
+    int w, h;
+    int bg_red, bg_blue, bg_green;
+    scanf("%d %d", &w, &h);
+    scanf("%d %d %d", &bg_red, &bg_green, &bg_blue);
+    printf("P3\n%d %d\n255\n", w, h);
+    for(int i = 0; i < h; ++i){
+        for(int j = 0; j < w; ++j){
+            printf("%d %d %d\n", bg_red, bg_green, bg_blue);
+        }
+    }
     return 0;
 }
---
+++ ProgrammingPractice1/Perkins_Nathan_PP_1.c
@@ -0,0 +1,136 @@
+
+
+/*
+ * Nathan Perkins
+ * nperk03@iastate.edu
+ * CPRE 185 Section 7
+ * Programming Practice 1
+         <Reflection 1 What were you trying to learn or achieve?>
+        My goal with this program was to get better at file IO in C.
+        This program is a basic database that allows you add entries in the formal of a title and entry body.
+        There isn't to much complex about this program. I would with the stdin and used fgets to assure that I can capture a full sentences with spaces.
+        <Reflection 2 Were you successful? Why or Why not?>
+        Yes I felt like I was successful.
+        There is a small bug where if you enter any characters besides numbers inside the commandline, the while loop turns infinite.
+        I did successfully work on my file IO and in the end it worked out very well.
+        <Reflection 3 Would you do anything differently if starting this program over?  If so, explain what.>
+        I do not think that I would do anything different if I started over. I outlined this program pretty well so I didn't really have an issue.
+        <Reflection 4 Think about the most important thing you learned when writing this piece of code.  What was it and explain why it was significant.>
+        I think that the most important thing that I learned from writing this code was the pre-code outlining.
+        Due to outlining the problem and finding the best way to solve it, the coding went much faster and there wasn't as much frustration
+        as there would have been if I didn't outline the program.
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Obtained the macro information from -> https://iq.opengenus.org/detect-operating-system-in-c/
+//Unistd documentation -> https://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html
+#if _WIN32
+int clear_screen(){return system("cls");}
+#else
+int clear_screen(){return system("clear");}
+
+#endif
+
+/*
+ * Color Codes -> https://www.theurbanpenguin.com/4184-2/
+ * More Color Codes -> https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
+ */
+
+void set_black(){printf("\033[0;30m");}
+void set_red(){printf("\033[0;31m");}
+void set_green(){printf("\033[0;32m");}
+void set_yellow(){printf("\033[0;33m");}
+void set_blue(){printf("\033[0;34m");}
+void set_purple(){printf("\033[0;35m");}
+void set_cyan(){printf("\033[0;36m");}
+void set_white(){printf("\033[0;37m");}
+void reset_color(){printf("\033[0m");}
+
+void title_blue(){printf("\033[0;93;1m");}
+
+void title_text(){
+    clear_screen();
+    printf("\n");
+    title_blue();
+    printf("BASIC DATABASE\n\n");
+    reset_color();
+}
+
+void options_text(){
+    reset_color();
+    set_green();
+    printf("\t[1] : Create New Entry\n");
+    printf("\t[2] : Read Entry\n");
+    printf("\t[3] : Exit\n");
+    printf("\n");
+    reset_color();
+}
+
+void create_new_entry(char * entry, char * title){
+    FILE * db = fopen("db.data", "a");
+    fprintf(db, "--------------------------------------------------------------------\n[Title]: %s\n[Entry]: %s\n--------------------------------------------------------------------\n", title, entry);
+    fclose(db);
+}
+
+void read_entries(){
+    FILE* db = fopen("db.data", "r");
+    if(!db){ reset_color();set_red();printf("\nNo Entries!\n\n");exit(1);}
+    char c;
+    while((c = fgetc(db)) != EOF){
+        printf("%c", c);
+    }
+
+    fclose(db);
+}
+
+void new_entry_text(){
+    fflush(stdin);
+    char title[256];
+    char entry[512];
+    printf("[Title]::> ");
+    fgets(title, 256, stdin);
+    title[strlen(title) - 1] = '\0';
+    printf("[Entry]::> ");
+    fgets(entry, 512, stdin);
+    entry[strlen(entry) - 1] = '\0';
+
+    create_new_entry(entry, title);
+}
+
+
+
+int main () {
+    int option = -1;
+    //Title
+    title_text();
+
+    while(option != 3){
+        printf("\n");
+        options_text();
+        reset_color();
+        set_cyan();
+        printf("Enter Option::> ");
+        scanf("%d", &option);
+        switch(option){
+            case 1:
+                new_entry_text();
+                break;
+            case 2:
+                read_entries();
+                break;
+            case 3:
+                printf("Exiting...\n");
+                break;
+            default:
+                reset_color();set_red();printf("Warning Invalid Entry!\n");reset_color();break;
+        }
+    }
+
+
+    return 0;
+}
